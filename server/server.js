bd8f9f2045c75be551cf61b080ebffe9b4db35b2
Tim Jahn <ceoymw@gmail.com>
2017-08-02 20:59:41 +0200

    editorconfig

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const fs = require("fs"),
-    EventEmitter = require("events").EventEmitter,
-    startServer = new EventEmitter();
+  EventEmitter = require("events").EventEmitter,
+  startServer = new EventEmitter();
 
 const http = require("http"),
-    express = require("express");
+  express = require("express");
 
 const app = express();
 app.use(express.static(__dirname + "/../client/"));
@@ -14,10 +14,10 @@ const server = http.createServer(app);
 const io = require("socket.io").listen(server);
 const port = 8000;
 
-server.listen(port, function() {
-    console.log("listening on *: " + port);
+server.listen(port, function () {
+  console.log("listening on *: " + port);
 });
 
-io.sockets.on("connection", function(client) {
-    // do socket.io stuff
+io.sockets.on("connection", client => {
+  // do socket.io stuff
 });
