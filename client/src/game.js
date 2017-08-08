5d5e1795b944d8aa395009caf81ff972f18dca1f
Michael Schertenleib <michaelschertenleib@gmail.com>
2017-08-08 09:09:24 +0200

    adjust pixi.Application invocation

--- client/src/game.js
+++ client/src/game.js
@@ -1,102 +1,107 @@
 define(["pixi", "connection", "constants", "player", "obstacles", "score"],
   function (pixi, connection, constants, Player, Obstacles, Score) {
     var app,
-    ticks = 0,
-    player,
-    obstacles,
-	  score,
-	  gamestate = 1,
-		gameover = new PIXI.Text('GAME OVER'),
-		start = new PIXI.Text('Click to start new game');
+      ticks = 0,
+      player,
+      obstacles,
+      score,
+      gamestate = 1,
+      gameover = new PIXI.Text('GAME OVER'),
+      start = new PIXI.Text('Click to start new game');
 
-	function init() {
-		// init connection
-		connection.init();
+    function init() {
+      // init connection
+      connection.init();
 
-		// create and configure application
-		app = new pixi.Application(constants.GAME_WIDTH, constants.game_HEIGHT, { backgroundColor: 0x1099bb });
-		app.renderer.view.style.position = "absolute";
-		app.renderer.view.style.display = "block";
-		app.stage.interactive = true;
-		document.body.appendChild(app.view);
-		app.stage.on("pointerdown", function () {
-			gamestate = 2;
-		});
+      // create and configure application
+      app = new pixi.Application({
+        width: constants.GAME_WIDTH,
+        height: constants.GAME_HEIGHT,
+        backgroundColor: 0x1099bb
+      });
 
-		// load images
-		pixi.loader
-			.add([
-				"images/hamster.png",
-				"images/background.png",
-				"images/box.png"
-			])
-			.load(function () {
-				
-				var background = new pixi.Sprite(pixi.utils.TextureCache["images/background.png"]);
-				player = new Player();
-				obstacles = new Obstacles(app);
-				score = new Score();
-				start.x = 320;
-				start.y = 350;
-				app.stage.addChild(start);
-				app.stage.addChild(background);
-				app.stage.addChild(player.sprite);
-				app.stage.addChild(score.text);
+      app.renderer.view.style.position = "absolute";
+      app.renderer.view.style.display = "block";
+      app.stage.interactive = true;
+      document.body.appendChild(app.view);
+      app.stage.on("pointerdown", function () {
+        gamestate = 2;
+      });
+
+      // load images
+      pixi.loader
+        .add([
+          "images/hamster.png",
+          "images/background.png",
+          "images/box.png"
+        ])
+        .load(function () {
+
+          var background = new pixi.Sprite(pixi.utils.TextureCache["images/background.png"]);
+          player = new Player();
+          obstacles = new Obstacles(app);
+          score = new Score();
+          start.x = 320;
+          start.y = 350;
+          app.stage.addChild(start);
+          app.stage.addChild(background);
+          app.stage.addChild(player.sprite);
+          app.stage.addChild(score.text);
 
 
-				gameLoop();
-			});
-	}
+          gameLoop();
+        });
+    }
 
-	function gameLoop() {
-		// loop this function 60 times per second
-		requestAnimationFrame(gameLoop);
-		console.log(gamestate);
-		if(gamestate == 0){
+    function gameLoop() {
+      // loop this function 60 times per second
+      requestAnimationFrame(gameLoop);
+      console.log(gamestate);
+      if (gamestate == 0) {
 
-			ticks += 1;
+        ticks += 1;
 
-			player.tick(ticks);
-			obstacles.tick(ticks);
-			score.tick(ticks);
-			
-			if (obstacles.collide(player.sprite)) {
-				console.log("collision");
-				gamestate = 1;
-				gameover.x = 320;
-				gameover.y = 350;
-				app.stage.addChild(gameover);
-				app.stage.removeAllListeners("pointerdown", function () {
-					player.jump();
-				});
-				app.stage.on("pointerdown", function () {
-					gamestate = 2;
-				});
-			}
+        player.tick(ticks);
+        obstacles.tick(ticks);
+        score.tick(ticks);
 
-			// render the stage
-			app.renderer.render(app.stage);
-		}
-		else if(gamestate == 1){
-			//game over or menu
-			app.renderer.render(app.stage);
-		}else if(gamestate == 2){
-			//restarting
-			app.stage.removeChild(gameover);
-			ticks = 0;
-			gamestate = 0;
-			obstacles.remove(app);
-			app.stage.removeAllListeners("pointerdown", function () {
-				gamestate = 2;
-			});
-			app.stage.on("pointerdown", function () {
-				player.jump();
-			});
-			
-		}
-	}
+        if (obstacles.collide(player.sprite)) {
+          console.log("collision");
+          gamestate = 1;
+          gameover.x = 320;
+          gameover.y = 350;
+          app.stage.addChild(gameover);
+          app.stage.removeAllListeners("pointerdown", function () {
+            player.jump();
+          });
+          app.stage.on("pointerdown", function () {
+            gamestate = 2;
+          });
+        }
 
-	return {
-		init: init
-	};
-});
+        // render the stage
+        app.renderer.render(app.stage);
+      }
+      else if (gamestate == 1) {
+        //game over or menu
+        app.renderer.render(app.stage);
+      } else if (gamestate == 2) {
+        //restarting
+        app.stage.removeChild(gameover);
+        ticks = 0;
+        gamestate = 0;
+        obstacles.remove(app);
+        app.stage.removeAllListeners("pointerdown", function () {
+          gamestate = 2;
+        });
+        app.stage.on("pointerdown", function () {
+          player.jump();
+        });
+
+      }
+    }
+
+    return {
+      init: init
+    };
+  });
